fbc6ac56db45b71120f4bbb5520caf39781cab1b
Abdoul Rahim Bah <abdoulrahimbah6@gmail.com>
2020-04-27 10:56:07 +0200

    Add hydrate method on DB class

--- www/core/DB.class.php
+++ www/core/DB.class.php
@@ -54,4 +54,16 @@ class DB
     public function populate()
     {
     }
+
+    public function hydrate(array $data)
+    {
+        if($data) { // Si $data n'est pas vide
+            foreach($data as $attName => $attValue) {
+                $settername = 'set'. ucfirst($attName);
+                if(method_exists($settername)) {
+                    $this->$settername($attValue);
+                }
+            }
+        }
+    }
 }
